38cf29f4f7e44ce0ea8287145fc9dd1301212c74
DaveyBoyCrocky <dgriffith@petsafe.net>
2017-02-07 13:48:05 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+app.use((request, response, next) => {
+  var now = new Date().toString();
+  var log = now + ": " + request.method + " " + request.url
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) { console.log('Unable to append to server.log'); }
+  })
+  next();
+});
+app.use((request, response, next) => {
+  response.render('maintenance.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: "Goobertown Gazette",
+    welcomeMessage: "Howdy there, Goobertownians!",
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: "About the Goobertown Gazette",
+  });
+});
+
+app.get('/bad', (request, response) => {
+  var error = {
+    errorMessage: "You failed to succeed successfully.",
+    gooberLevel: 42
+  };
+  response.send(error);
+})
+
+app.listen(3000, () => {
+  console.log('==Server is up on port 3000.');
+});
