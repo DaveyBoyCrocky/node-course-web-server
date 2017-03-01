5a7019924f0bb24f3ba6547aaa0021799b49adb4
DaveyBoyCrocky <dgriffith@petsafe.net>
2017-03-01 11:31:46 -0500

    No maintentance mode #2

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use((request, response, next) => {
   })
   next();
 });
-app.use((request, response, next) => {
+//app.use((request, response, next) => {
   //response.render('maintenance.hbs');
-});
+//});
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
